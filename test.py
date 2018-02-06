9a577bec15620d05bc61b86d4197ceb6908ea117
OrangeJulian <marks.julian.e@google.com>
2018-02-05 19:10:30 -0800

    Update

--- test.py
+++ test.py
@@ -5,26 +5,34 @@ import pyperclip    # To help with receiving long text responses to local machin
 import os
 
 ### Important URLs
-API_KEY = "RGAPI-eaca5c50-8809-47da-a474-02807152917c"
+API_KEY = "RGAPI-59a50598-2258-408b-ad72-421643a011a6"
 CHAMPIONS = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json"
 SPECTATOR_FEAT = "https://na1.api.riotgames.com/lol/spectator/v3/featured-games?api_key="
-TEST = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/mastershark101?api_key="
 
+BASE_URL = "https://na1.api.riotgames.com/lol/"
+SUMMONER_ID = "summoner/v3/summoners/by-name/Bjergsen?api_key="
+MATCH_ID = "match/v3/matchlists/by-account/214376669?api_key="
+GAME_ID = "match/v3/matches/2707683651?api_key="
 
-GET = TEST+API_KEY
-
-
-
+GET = BASE_URL+SUMMONER_ID+API_KEY
 
 print('-------------------- Begin Program -------------------------\n')
-STATE = requests.get(GET)
 
+STATE = requests.get(GET)
 print(STATE.text)
 
-CHAMPS = requests.get(CHAMPIONS)    # Response is json lib of all champions and their personal game information
+GET = BASE_URL+MATCH_ID+API_KEY
+MATCHES = requests.get(GET)
+print(MATCHES.text)
 
-STATE = SPECTATOR_FEAT + API_KEY
-SPEC = requests.get(STATE)
-print(SPEC.text)
+GET = BASE_URL+GAME_ID+API_KEY
+GAME = requests.get(GET)
+print(GAME.text)
+
+# CHAMPS = requests.get(CHAMPIONS)    # Response is json lib of all champions and their personal game information
+#
+# STATE = SPECTATOR_FEAT + API_KEY
+# SPEC = requests.get(STATE)
+# print(SPEC.text)
 
 print('-------------------- End Program -------------------------\n')
