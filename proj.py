43a450b90a7103973e0a5b701b29dd5830fcb280
OrangeJulian <marks.julian.e@google.com>
2018-04-08 11:46:35 -0700

    Regression Update with Results

--- proj.py
+++ proj.py
@@ -21,8 +21,7 @@ INFO = 'C:/users/julian/school/programming/projects/league-analysis/data/Leagueo
 LOL = pd.read_csv(INFO)
 
 
-
-### Helper Functions
+### Helper Functions ###
 
 Champion = [x.lower() for x in ["Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Ashe","AurelionSol","Azir","Bard","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","ChoGath","Corki","Darius","Diana","DrMundo","Draven","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","JarvanIV","Jax","Jayce","Jhin","Jinx","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","KhaZix","Kindred","Kled","KogMaw","LeBlanc","LeeSin","Leona","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","MasterYi","MissFortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Nidalee","Nocturne","Nunu","Olaf","Orianna","Ornn","Pantheon","Poppy","Quinn","Rakan","Rammus","RekSai","Renekton","Rengar","Riven","Rumble","Ryze","Sejuani","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Syndra","TahmKench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","TwistedFate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","VelKoz","Vi","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah","Xerath","XinZhao","Yasuo","Yorick","Zac","Zed","Ziggs","Zilean","Zoe","Zyra"]]
 
@@ -45,41 +44,46 @@ def WhichLane(x):
     else:
         print("Error: Invalid Input")
 
-def ConvertGold(rb, lane):  # First cap for lane, not for rb
-    rb = rb.lower()
-    if rb=='red':
-        br = 'blue'
-    else:
-        br = 'red'
-    yGet = 'gold'+rb+lane
-    champ = LOL[rb+lane+'Champ']
-    Ochamp = LOL[br+lane+'Champ']
-    convert = LOL[yGet]
-    converted = pd.DataFrame()
-    final = pd.DataFrame()
-    for i in range(0,convert.shape[0]):
-        String = convert.iloc[i][1:-1]
-        x = pd.DataFrame(list(map(int,String.split(', ')))).T
-        converted = converted.append(x, ignore_index=True)
 
-    # Change the range on this loop to cycle through more than 1 game.
-    for i in range(0,len(converted.index)):   # This is the real loop. The replacement uses less data/time.
-    # for i in range(0,50):
-        y = converted.iloc[i]
-        y = y.dropna(how='any')
-        const = pd.Series([y[0]]*len(y))
-        t = pd.Series(list(range(0,len(y))))
+# This is difference of gold regression by (redLaneGold-blueLaneGold).
+def ConvertGold(lane):  # First cap for lane, not for rb
+    yr = 'goldred'+lane
+    yb = 'goldblue'+lane
+    champ = LOL['red'+lane+'Champ']
+    Ochamp = LOL['blue'+lane+'Champ']
+    convert1 = LOL[yr]
+    convert2 = LOL[yb]
+    converted1 = pd.DataFrame()
+    converted2 = pd.DataFrame()
+    for i in range(0,convert1.shape[0]):
+        String = convert1.iloc[i][1:-1]
+        x = pd.DataFrame(list(map(int,String.split(', ')))).T
+        converted1 = converted1.append(x, ignore_index=True)
+        String = convert2.iloc[i][1:-1]
+        x = pd.DataFrame(list(map(int,String.split(', ')))).T
+        converted2 = converted2.append(x, ignore_index=True)
+
+    final = pd.DataFrame()
+    for i in range(0,len(converted1.index)):   # This is the real loop. The replacement uses less data/time.
+    # for i in range(0,5):
+        yr = converted1.iloc[i]
+        yr = yr.dropna(how='any')
+        yb = converted2.iloc[i]
+        yb = yb.dropna(how='any')
+        t = pd.Series(list(range(0,len(yr))))
         t2 = t**2
-        x1 = pd.Series([WhichLane(lane)]*len(y))
-        x2 = pd.Series([WhoChamp(champ[i])]*len(y))
-        x3 = pd.Series([WhoChamp(Ochamp[i])]*len(y))
-        merge = pd.concat([y,const,t,t2,x1,x2,x3], axis=1)
-        merge.columns = ['Gold','Constant','Time','Time^2','Lane','Champ','OppChamp'] 
+        x1 = pd.Series([WhichLane(lane)]*len(yr))
+        x2 = pd.Series([WhoChamp(champ[i])]*len(yr))
+        x3 = pd.Series([WhoChamp(Ochamp[i])]*len(yr))
+        merge = pd.concat([yr,yb,t,t2,x1,x2,x3], axis=1)
+        merge.columns = ['RedGold','BlueGold','Time','Time^2','Lane','Champ','OppChamp'] 
         final = final.append(merge)
     return final
 
 
-### Main Program
+
+
+### Preprocessing ###
 
 print('----------------------- Begin Program -----------------------')
 timeStart = time.time()
@@ -90,38 +94,67 @@ pd.set_option('display.max_rows',None)
 # Possibility to add a factor which considers difference between games.
 
 time1 = time.time()
-RedTop = ConvertGold('red','Top')
+
+RedTop = ConvertGold('Top')
+
+# This part is extremely compacted to test if all functions above work properly, and to see if regression programming runs bug free.
+# ##########################################################################################
+
+# print(RedTop)
+# print(RedTop.shape)
+# print(RedTop.columns)
+
+# Total = RedTop
+
+
+# LaneChange = pd.get_dummies(Total['Lane'])
+# for i in LaneChange.columns:
+# 	LaneChange.rename(columns={int(i):WhichLane(int(i))}, inplace=True)
+
+
+# OChampChange = pd.get_dummies(Total['OppChamp'])
+
+# ChampChange = pd.get_dummies(Total['Champ'])
+
+
+# for i in ChampChange.columns:
+# 	ChampChange.rename(columns={int(i):WhoChamp(int(i))}, inplace=True)
+
+# for i in OChampChange.columns:
+# 	x=WhoChamp(int(i))+'Opp'
+# 	OChampChange.rename(columns={int(i):x}, inplace=True)
+
+# ChampChange = pd.concat([ChampChange, OChampChange], axis=1)
+
+# Total = pd.concat([Total.drop(['Lane','Champ','OppChamp'], axis=1), LaneChange, ChampChange], axis=1)
+
+
+# target = Total['GoldDiff']
+# predictor = Total.drop(['GoldDiff'], axis=1)
+
+# fit = sm.OLS(target,predictor).fit()
+# print(fit.summary())
+
+# sys.exit()
+
+
+# ######################################################################################
 
 time2 = time.time()
-RedJungle = ConvertGold('red','Jungle')
-RedMid = ConvertGold('red','Middle')
-RedADC = ConvertGold('red','ADC')
-RedSupp = ConvertGold('red','Support')
 
-time3 = time.time()
+RedJungle = ConvertGold('Jungle')
+RedMid = ConvertGold('Middle')
+RedADC = ConvertGold('ADC')
+RedSupp = ConvertGold('Support')
 RedAll = [RedTop, RedJungle, RedMid, RedADC, RedSupp]
 
-time4 = time.time()
+time3 = time.time()
 
-BlueTop = ConvertGold('blue','Top')
-BlueJungle = ConvertGold('blue','Jungle')
-BlueMid = ConvertGold('blue','Middle')
-BlueADC = ConvertGold('blue','ADC')
-BlueSupp = ConvertGold('blue','Support')
-BlueAll = [BlueTop, BlueJungle, BlueMid, BlueADC, BlueSupp]
-
-time5 = time.time()
-
-Total = pd.DataFrame()
-Total = Total.append(RedAll)
-Total = Total.append(BlueAll)
-
+Total = pd.concat(RedAll, axis=0)
 
 LaneChange = pd.get_dummies(Total['Lane'])
 for i in LaneChange.columns:
 	LaneChange.rename(columns={int(i):WhichLane(int(i))}, inplace=True)
-
-
 
 
 # Incorrect merging of data. All missing information is converted to NA instead of 0 dummies (for missing champs/lanes)
@@ -135,7 +168,7 @@ for i in LaneChange.columns:
 
 ## FIXED: Now instead creates a Champ and OppChamp Dataframe, concatenates them horizontally and thus each Champ and OppChamp have independent dummy variables. Currently Opp is set to -1 to "Reduce" gold gain from the player.
 
-OChampChange = (-1)*pd.get_dummies(Total['OppChamp'])
+OChampChange = pd.get_dummies(Total['OppChamp'])
 
 ChampChange = pd.get_dummies(Total['Champ'])
 
@@ -150,31 +183,15 @@ for i in OChampChange.columns:
 
 ChampChange = pd.concat([ChampChange, OChampChange], axis=1)
 
-print(ChampChange.head())
-print(list(ChampChange.columns))
-
-
-
 Total = pd.concat([Total.drop(['Lane','Champ','OppChamp'], axis=1), LaneChange, ChampChange], axis=1)
 
 # Total = Total.drop(['Lane','Champ','OppChamp'], axis=1).merge(LaneChange)
 
-time6 = time.time()
-
-singleTime = time2-time1
-teamTime = time3-time1
-totTeamTime = time4-time1
-totTime = time5-time1
-lastTime = time6-time5
-fullTime = time6-time1
-
-allTime = [singleTime, teamTime, totTeamTime, totTime, lastTime, fullTime]
-
-x=1
-for i in allTime:
-	print('Time'+str(x))
-	print(i)
-	x=x+1
+# x=1
+# for i in allTime:
+# 	print('Time'+str(x))
+# 	print(i)
+# 	x=x+1
 
 
 
@@ -187,17 +204,69 @@ for i in allTime:
 
 
 
+### Main Program ###
+
+usr=-1
+while(usr!=0):
+	print("What results would you like to see?")
+	print("1: Red - Blue Gold Difference Regression")
+	print("2: Red Gold Regression")
+	print("3: Blue Gold Regression")
+	print("4: Red Lane Gold Regression")
+	print("5: Blue Lane Gold Regresion")
+	print("6: Lane Gold Regression")
+	print("7: Lane Gold Difference Regression (Red - Blue)")
+	usr=int(input("Enter 0 to exit"))
+	if(usr==1):
+		target = Total['RedGold']-Total['BlueGold']
+		predictor = Total.drop(['RedGold','BlueGold'], axis=1)
+		fit = sm.OLS(target,predictor).fit()
+		print(fit.summary())
+	if(usr==2):
+		target = Total['RedGold']
+		predictor = Total.drop(['RedGold','BlueGold'], axis=1)
+		fit = sm.OLS(target,predictor).fit()
+		print(fit.summary())
+	if(usr==3):
+		target = Total['BlueGold']
+		predictor = Total.drop(['RedGold','BlueGold'], axis=1)
+		fit = sm.OLS(target,predictor).fit()
+		print(fit.summary())
+	if(usr==4):
+		target = Total['RedGold']
+		predictor = Total.drop(['RedGold','BlueGold'], axis=1)
+		fit = sm.OLS(target, predictor).fit()
+		print(fit.summary())
+	if(usr==5):
+		target = Total['BlueGold']
+		predictor = Total.drop(['RedGold','BlueGold'], axis=1)
+		fit = sm.OLS(target, predictor).fit()
+		print(fit.summary())
+	if(usr==6):
+		target = pd.concat([Total['RedGold'],Total['BlueGold']],axis=0)
+		predictor = pd.concat([Total[['Time','Time^2','Top','Jungle','Middle','Adc','Support']],Total[['Time','Time^2','Top','Jungle','Middle','Adc','Support']]],axis=0)
+		# pd.concat([Total.drop(['RedGold','BlueGold'],axis=1),Total.drop(['RedGold','BlueGold'],axis=1)],axis=0)
+		fit = sm.OLS(target, predictor).fit()
+		print(fit.summary())
+	if(usr==7):
+		target = Total['RedGold']-Total['BlueGold']
+		predictor = Total[['Time','Time^2','Top','Jungle','Middle','Adc','Support']]
+		fit = sm.OLS(target, predictor).fit()
+		print(fit.summary())
+
+
+
 # For estimation, starting gold is known, passive 'Gold per Minute' is known. All other gold is from cs (sub for time), kills, items or masteries, so these are the qualities to be studied. To find best results, find a way to account for these in the model (Maybe remove their effect, or at least add it to the known regression equation)
 # If time variable is limited to the passive gold income/time then the entire gold effect can be carried in the weight of the champion variable.
 
-target = Total['Gold']
-predictor = Total.drop(['Gold'], axis=1)
+# target = Total['GoldDiff']
+# predictor = Total.drop(['GoldDiff'], axis=1)
 
-fit = sm.OLS(target,predictor).fit()
-print(fit.summary())
+# fit = sm.OLS(target,predictor).fit()
+# print(fit.summary())
 
-predictions = fit.predict(predictor)
-xfit = np.linspace(0,10,1000)
+# predictions = fit.predict(predictor)
+# xfit = np.linspace(0,10,1000)
 
 # x = sm.add_constant(predictor)  # This adds the intercept estimate when other lanes are added into the data matrix.
 # fit0 = sm.OLS(target,x).fit()
@@ -236,5 +305,4 @@ xfit = np.linspace(0,10,1000)
 # print(LOL[['blueTopChamp','goldblueTop','redTopChamp','goldredTop']])
 
 timeEnd = time.time()
-print(timeEnd)
 print('----------------------- End Program. Run Time: {} -----------------------'.format(timeEnd-timeStart))
